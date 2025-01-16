d41c12953b909196a035a88b0cf100af149fc503
Ayush shaw <86004647+rge5@users.noreply.github.com>
2025-01-16 06:30:56 +0000

    conversionofdatatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,20 @@
+let score = 33
+
+//console.log(typeof score)
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+let isLoggedIn = "Ayush"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1=>true; 0 =>false
+//"" => false
+//"Ayush" =>true
+
+let someNumber = 33
+let StringNumber = String(someNumber)
+console.log(StringNumber)
