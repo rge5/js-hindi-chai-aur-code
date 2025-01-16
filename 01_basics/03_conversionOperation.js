1f41820663bd54f11e3d6d2dc8fea5303b3bf09c
Ayush shaw <86004647+rge5@users.noreply.github.com>
2025-01-16 06:56:00 +0000

    conversionoperation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -17,4 +17,41 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let someNumber = 33
 let StringNumber = String(someNumber)
-console.log(StringNumber)
+//console.log(StringNumber)
+
+// *********************************** Operations **************************************
+
+let value = 3
+let negValue = -value
+//console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/2)
+// console.log(2%2)
+
+let str1 = "hello"
+let str2 = "Ayush"
+
+
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 3)
+// console.log(1 + 2 + "3")
+
+// console.log(+true)
+// console.log(+"")
+
+let num1, num2, num3
+
+num1 = num2 =num3 = 2+ 2
+console.log(num1, num2, num3)
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter)
