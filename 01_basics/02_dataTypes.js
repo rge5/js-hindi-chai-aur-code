3d0ae95fa17b8fda55b7308cf332f4dbf64cb3be
Ayush shaw <86004647+rge5@users.noreply.github.com>
2025-01-16 06:07:01 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+ "use strict"; // treat all JS code as newer version
+
+//  alert( 3 + 3) //we are using nodejs, not browser
+
+console.log(3
+    +3) // code readability ishould be high
+
+    let nome= "Ayush" //string
+    let age = 23 //number
+    let isLoggedIn = false //boolean
+    let state;
+
+    // number => 2 to power 53
+    // bigint
+    // string =>""
+    // boolean => true/false
+    // null => standalone value
+    // undefined =>
+    // symbol => unique
+
+    // object
+
+    console.log(typeof age);
+    console.log(typeof null);
+    console.log(typeof undefined);
