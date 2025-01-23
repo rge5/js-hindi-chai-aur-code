068623048ccba2a317548906b9a214d89f45ad9b
Ayush shaw <86004647+rge5@users.noreply.github.com>
2025-01-23 06:22:03 +0000

    dataTypes-summary

---
+++ 01_basics/dataTypes-summary.js
@@ -0,0 +1,44 @@
+// premitive
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt.
+
+// const score= 100
+// const scoreValue= 100.3
+
+// const isLoggedIn = false
+// const outsidetype = null
+// let userEmail;
+
+// const id = symbol('123')
+// const anotherId = symbol('123')
+
+// console.log(id === anotherId);
+
+// const bigNumber = 321654211565n
+
+// ReferenceError (Non premitive)
+
+//Array, Objects, Functions.
+
+// const heroes = ["shaktiman","nagraaj", "doga" ]
+// let myObj = {
+//     name:"Ayush",
+//     age: 22,
+// }
+
+// const myFunction = function(){
+// console.log("Hello World");
+// }
+
+// console.log(typeof scoreValue)
+
+// *********************************************************************************************
+
+// Stack(premitive) , Heap(Non-premitive)
+
+let myYoutubeName = "ayushshaw.com"
+let anotherName = myYoutubeName
+anotherName = "Ayush"
+console.log(anotherName);
+console.log(myYoutubeName)
+
